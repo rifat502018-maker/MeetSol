f64b1b8e3a0b297c427b7807bf9e373aed20e6a5
rifat502018-maker <rifat502018@gmail.com>
2026-06-01 12:20:44 +0300

    Create MainActivity.kt

---
+++ app/src/main/java/com/nexiosoft/meetsol/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.nexiosoft.meetsol
+
+import android.annotation.SuppressLint
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.webkit.ValueCallback
+import android.webkit.WebChromeClient
+import android.webkit.WebSettings
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var webView: WebView
+    private var fileUploadCallback: ValueCallback<Array<Uri>>? = null
+
+    private val fileChooserLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+        if (fileUploadCallback == null) return@registerForActivityResult
+        val results: Array<Uri>? = if (result.resultCode == Activity.RESULT_OK) {
+            val dataString = result.data?.dataString
+            if (dataString != null) arrayOf(Uri.parse(dataString)) else null
+        } else {
+            null
+        }
+        fileUploadCallback?.onReceiveValue(results)
+        fileUploadCallback = null
+    }
+
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        webView = findViewById(R.id.webView)
+
+        val webSettings: WebSettings = webView.settings
+        webSettings.javaScriptEnabled = true
+        webSettings.domStorageEnabled = true 
+        webSettings.allowFileAccess = true
+        webSettings.allowContentAccess = true
+        webSettings.mediaPlaybackRequiresUserGesture = false 
+
+        webView.webViewClient = object : WebViewClient() {
+            @Deprecated("Deprecated in Java")
+            override fun shouldOverrideUrlLoading(view: WebView?, url: String?): Boolean {
+                view?.loadUrl(url ?: "")
+                return true
+            }
+        }
+
+        webView.webChromeClient = object : WebChromeClient() {
+            override fun onShowFileChooser(
