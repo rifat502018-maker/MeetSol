68b3897f184f6848924e2349037c66b418e2cdbb
rifat502018-maker <rifat502018@gmail.com>
2026-06-01 16:36:37 +0300

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.nexiosoft.meetsol"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
+        versionCode = 2.0.1
         versionName = "1.0"
     }
 
